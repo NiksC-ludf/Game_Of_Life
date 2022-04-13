f6689260c3a9c5b1148d648b4ba2b14a068017a0
Niks_Cibulis <niks.cibulis@accenture.com>
2022-04-13 14:40:26 +0300

    Added functionality to set live cells.

--- Program.cs
+++ Program.cs
@@ -16,6 +16,42 @@ public class Program
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine();
+        }
+        void FillDefault(int[,] args)
+        {
+            int num = 1;
+            for (int i = 0; i < args.GetLength(0); i++)
+            {
+                for (int j = 0; j < args.GetLength(1); j++)
+                {
+                    args[i, j] = num;
+                    num++;
+                }
+            }
+        }
+        void MarkAlive(int[,] args, int[] nums)
+        {
+            int num = 0;
+            int max = nums.GetLength(0);
+            for(int i = 0; i < args.GetLength(0); i++)
+            {
+                for( int j = 0; j < args.GetLength(1); j++)
+                {
+                    if(args[i, j] == nums[num])
+                    {
+                        args[i, j] = 1;
+                        if(!(num == max || num+1 == max))
+                        {
+                            num++;
+                        }
+                    }
+                    else
+                    {
+                        args[i, j] = 0;
+                    }
+                }
+            }
         }
         Console.WriteLine("Game of Life!");
         Console.WriteLine("Please input the amount of columns for your field: ");
@@ -24,15 +60,19 @@ public class Program
         int rows = Convert.ToInt32(Console.ReadLine());
 
         int[,] data = new int[rows, columns];
-        int num = 1;
-        for (int i = 0; i < data.GetLength(0); i++)
+        FillDefault(data);
+        Print(data);
+
+        Console.WriteLine("How many alive cells would you like?"); // Maybe would be better to use default size rows*columns
+        int InputSize = Convert.ToInt32(Console.ReadLine());
+        int[] input = new int[InputSize];
+        Console.WriteLine("Input the numbers where you want alive cells:");
+        for (int i = 0; i < InputSize; i++)
         {
-            for (int j = 0; j < data.GetLength(1); j++)
-            {
-                data[i, j] = num;
-                num++;
-            }
+            input[i] = Convert.ToInt32(Console.ReadLine());
         }
+        Array.Sort(input);
+        MarkAlive(data, input);
         Print(data);
     }
 }
