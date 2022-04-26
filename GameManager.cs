5a0ca13fdebff7579007cb1cf82eab8e746b9e6c
Niks_Cibulis <niks.cibulis@accenture.com>
2022-04-26 13:21:43 +0300

    Moved object creation inside method.

--- GameManager.cs
+++ GameManager.cs
@@ -13,16 +13,17 @@ namespace GameOfLife
     {
         private const string inputColumnMessage = "Input amount of columns.";
         private const string inputRowMessage = "Input amount of rows.";
-        private static UserInterface userInterface = new UserInterface();
-        private static int column = userInterface.GetValueInRange(inputColumnMessage, 5, 200);
-        private static int row = userInterface.GetValueInRange(inputRowMessage, 5, 200);
-        private GameData gameData = new GameData(row, column);
 
         /// <summary>
         /// Starts the game.
         /// </summary>
         public void Play()
         {
+            UserInterface userInterface = new UserInterface();
+            int column = userInterface.GetValueInRange(inputColumnMessage, 5, 200);
+            int row = userInterface.GetValueInRange(inputRowMessage, 5, 200);
+            GameData gameData = new GameData(row, column);
+
             while (true)
             {
                 gameData.GetNewGeneration();
